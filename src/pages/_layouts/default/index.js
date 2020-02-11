c17acb44ca800d9900bb5d6bef84787afd27b9e4
Luis Fernando <lsh.fernandors@gmail.com>
2020-02-11 19:29:23 -0300

    implementing dynamically menu

--- src/pages/_layouts/default/index.js
+++ src/pages/_layouts/default/index.js
@@ -1,11 +1,26 @@
-import React from 'react'
-import Link from '@material-ui/core/Link';
+import React, { useEffect, useState } from 'react'
 
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    IconButton,
+    Drawer,
+    Divider,
+    List,
+    ListItem,
+    ListItemText
+ } from '@material-ui/core';
+
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+
+import api from '../../../services/api';
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -18,29 +33,87 @@ const useStyles = makeStyles(theme => ({
         flexGrow: 1,
         textAlign: 'center'
     },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+      },
+      drawerPaper: {
+        width: drawerWidth,
+      },
+      drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+      }
 }));
 
 export default function DefaultLayout({ children }) {
+    const [open, setOpen] = useState(false);
+    const [menus, setMenus] = useState([]);
     const classes = useStyles();
+    const theme = useTheme();
+
+    async function loadRotas() {
+        const response = await api.get('/rotas');
+
+        setMenus(response.data);
+    }
+
+    useEffect(() => {
+        loadRotas();
+    }, [])
+
+    function handleDrawerOpen() {
+        setOpen(true);
+    }
+
+    function handleDrawerClose() {
+        setOpen(false);
+    }
 
     return (
         <>
             <div className={classes.root}>
                 <AppBar position="static">
                     <Toolbar>
-                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={handleDrawerOpen}
+                            edge="start"
+                        >
+                            <MenuIcon />
                         </IconButton>
-                        <Typography variant="h4" className={classes.title} href="/">
+                        <Typography variant="h4" className={classes.title}>
                             Menu
                         </Typography>
-                        <Link color="inherit" href="/">
-                            MAIN
-                        </Link>
-                        <Link color="inherit" href="/cadastrar">
-                            ADD
-                        </Link>
                     </Toolbar>
                 </AppBar>
+                <Drawer
+                    className={classes.drawer}
+                    variant="persistent"
+                    anchor="left"
+                    open={open}
+                    classes={{
+                        paper: classes.drawerPaper,
+                    }}
+                >
+                    <div className={classes.drawerHeader}>
+                        <IconButton onClick={handleDrawerClose}>
+                            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                        </IconButton>
+                    </div>
+                    <Divider />
+                    <List>
+                        {menus && menus.map(menu => (
+                            <ListItem button component="a" href={menu.caminho} key={menu.id}>
+                                <ListItemText primary={menu.nome} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </Drawer>
             </div>
             <div>{children}</div>
         </>
