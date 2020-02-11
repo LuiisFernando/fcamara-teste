f3f444b1d198ddf53810a7eb1ddacbddb66f8eeb
Luis Fernando <lsh.fernandors@gmail.com>
2020-02-10 22:36:14 -0300

    removing variable and method not used

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Switch } from 'react-router-dom';
 
 import Route from './Route';
@@ -6,19 +6,7 @@ import Route from './Route';
 import Dashboard from '../pages/Dashboard';
 import Cadastrar from '../pages/Cadastrar';
 
-import api from '../services/api';
-
 export default function Routes() {
-
-    useEffect(() => {
-        async function loadRotas() {
-            const response = await api.get('rotas');
-
-            console.log(response.data)
-        }
-
-        //loadRotas();
-    }, []);
 
     return (
         <Switch>
